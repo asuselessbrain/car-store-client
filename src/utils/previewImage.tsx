a396dae71f92ef87398bef7c9fdf24c454227d96
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-06-21 10:53:29 +0600

    add: delete image form image preview

--- src/utils/previewImage.tsx
+++ src/utils/previewImage.tsx
@@ -1,3 +1,5 @@
+import { RxCross2 } from "react-icons/rx";
+
 interface IProps {
     setImage: React.Dispatch<React.SetStateAction<File[] | []>>;
     preview: string[] | [];
@@ -6,9 +8,21 @@ interface IProps {
 
 
 export const ImagePreview = ({ setImage, preview, setPreview }: IProps) => {
+
+    const handleDelete = (index: number) => {
+        setImage((prev) => prev?.filter((_, indx) => indx !== index));
+        setPreview((prev) => prev?.filter((_, indx) => indx !== index))
+    }
     return (
-        <div>
-            {preview?.map((prev, index) => <img key={index} src={prev} alt="profile Image" height={145} width={145} />)}
+        <div className="relative">
+            {
+
+                preview?.map((prev, index) => (<><img key={index} src={prev} alt="profile Image" height={145} width={145} />
+                    <RxCross2 size={28} onClick={() => handleDelete(index)} className="bg-red-500 text-white font-bold rounded-full absolute -top-2 -right-2 hover:bg-red-400 hover:cursor-pointer p-1" />
+                </>))
+            }
+
+
         </div>
     )
 }
