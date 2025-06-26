a7a8c10dbf347a7787d067273efc0ef4547319ae
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-06-26 08:02:53 +0600

    delete: testmonial section from home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,11 @@
 import { Banner } from "./components/Banner";
 import FeaturedProducts from "./components/Featured";
-import Responsive from "./components/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <FeaturedProducts />
-      <Responsive />
     </div>
   );
 };
